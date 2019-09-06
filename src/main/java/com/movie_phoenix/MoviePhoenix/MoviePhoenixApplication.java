3c22b85acddf52534790ed21a37b3c50d1d99bf6
Ben Feinstein <ben.e.feinstein@gmail.com>
2019-09-06 13:00:56 -0400

    initial commit

---
+++ src/main/java/com/movie_phoenix/MoviePhoenix/MoviePhoenixApplication.java
@@ -0,0 +1,13 @@
+package com.movie_phoenix.MoviePhoenix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviePhoenixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviePhoenixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movie_phoenix/MoviePhoenix/MoviePhoenixApplicationTests.java
@@ -0,0 +1,16 @@
+package com.movie_phoenix.MoviePhoenix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoviePhoenixApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
