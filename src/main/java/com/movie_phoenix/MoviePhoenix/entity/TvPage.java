55fd9d0f6e703b834a4f3c150dc49a70302eed46
Atiba <tiba176@gmail.com>
2019-09-09 13:58:27 -0400

    Tv Show revised

--- src/main/java/com/movie_phoenix/MoviePhoenix/entity/TvPage.java
+++ src/main/java/com/movie_phoenix/MoviePhoenix/entity/TvPage.java
@@ -14,4 +14,50 @@ public class TvPage {
 	private String imageUrl;
 	private String overview;
 	
+	public TvPage() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFirstAirDate() {
+		return firstAirDate;
+	}
+
+	public void setFirstAirDate(String firstAirDate) {
+		this.firstAirDate = firstAirDate;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public String getOverview() {
+		return overview;
+	}
+
+	public void setOverview(String overview) {
+		this.overview = overview;
+	}
+	
+	
 }
