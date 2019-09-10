12a13dacd63237a3ef068ec8856d0925acb542db
Ben Feinstein <ben.e.feinstein@gmail.com>
2019-09-10 13:22:15 -0400

    trying stuff

--- src/main/java/com/movie_phoenix/MoviePhoenix/service/GoogleService.java
+++ src/main/java/com/movie_phoenix/MoviePhoenix/service/GoogleService.java
@@ -1,28 +1,26 @@
 package com.movie_phoenix.MoviePhoenix.service;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 @Component // this helps us use Spring's Dependency Injection
 public class GoogleService {
 
 	
-	private static final String OUR_URL = "http://localhost:8080/";
-	/*
-	 * The @Value annotation allows us to pull values from the
-	 * application.properties
-	 * 
-	 * Remember to add the application.properties to .gitignore before working on
-	 * the application
-	 */
-	@Value("${google.client_secret}")
-	String clientSecret;
-
-	
-	@Value("${google.client_id}")
-	String clientId;
-	RestTemplate rt = new RestTemplate();
+//	private static final String OUR_URL = "http://localhost:8080/";
+//	/*
+//	 * The @Value annotation allows us to pull values from the
+//	 * application.properties
+//	 * 
+//	 * Remember to add the application.properties to .gitignore before working on
+//	 * the application
+//	 */
+//	@Value("${google.client_secret}")
+//	String clientSecret;
+//
+//	
+//	@Value("${google.client_id}")
+//	String clientId;
+//	RestTemplate rt = new RestTemplate();
 
 	
 }
