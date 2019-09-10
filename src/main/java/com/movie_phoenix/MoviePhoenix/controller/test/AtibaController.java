bcb1a0afbf2dfc49c1cd6f24c3efff913db7b631
Ben Feinstein <ben.e.feinstein@gmail.com>
2019-09-10 13:19:27 -0400

    unused import

--- src/main/java/com/movie_phoenix/MoviePhoenix/controller/test/AtibaController.java
+++ src/main/java/com/movie_phoenix/MoviePhoenix/controller/test/AtibaController.java
@@ -1,8 +1,5 @@
 package com.movie_phoenix.MoviePhoenix.controller.test;
 
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
