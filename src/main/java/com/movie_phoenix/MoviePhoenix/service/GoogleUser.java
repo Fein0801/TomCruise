06875c295f63d0f03898482edddba671ec7ec9b2
Kbusch54 <kevinbusch@comcast.net>
2019-09-12 10:05:29 -0400

    eclipse is crazy

--- src/main/java/com/movie_phoenix/MoviePhoenix/service/GoogleUser.java
+++ src/main/java/com/movie_phoenix/MoviePhoenix/service/GoogleUser.java
@@ -1,7 +1,52 @@
 package com.movie_phoenix.MoviePhoenix.service;
 
+
 public class GoogleUser {
+
 	private Integer entryId;
 	private String email;
+	private String name;
+
+	public GoogleUser() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public GoogleUser(Integer entryId, String email, String name) {
+		super();
+		this.entryId = entryId;
+		this.email = email;
+		this.name = name;
+	}
+
+	public GoogleUser(String email, String name) {
+		super();
+		this.email = email;
+		this.name = name;
+	}
+
+	public Integer getEntryId() {
+		return entryId;
+	}
+
+	public void setEntryId(Integer entryId) {
+		this.entryId = entryId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
